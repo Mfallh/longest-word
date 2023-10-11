212a361b9130f8e11ded04aafe35e351baf12f31
marius fall <marius.fall@gmail.com>
2023-10-11 12:33:55 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,31 @@
+import random
+import string
+from collections import Counter
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = random.choices(string.ascii_uppercase, k=9)
+
+
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+
+        letters = self.grid.copy()
+        counter_list = Counter(letters)
+        counter_word = Counter(word)
+        bool_list = []
+        bool_list.append(len(word) <= 9)
+        bool_list.append(type(word) == str)
+        for letter in word:
+            bool_list.append(letter not in string.digits)
+        for letter in set(word):
+            bool_list.append(letter in letters)
+        for letter in set(word):
+            counter_word[letter] <= counter_list[letter]
+
+        if all(bool_list):
+            return True
+        else:
+            return False
